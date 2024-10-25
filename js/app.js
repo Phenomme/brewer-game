0f1f546ba3ea1135453ca70563c2d93449d7e581
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-10-24 18:14:23 -0700

    jonas version

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 const bodyElement = document.querySelector("body")
 const page1h1Element = document.createElement("h1")
-const  inputElement = document.createElement("input")
+const pageh2Element = document.createElement("h2")
+const inputElement = document.createElement("input")
 const addNameButton = document.createElement("button")
-
+const h3Question = document.createElement('h3')
+const optionButton1 = document.createElement('button')
+const optionButton2 = document.createElement('button')
+const buttonContainer = document.createElement('div')
+    
 
 
 
 /*-------------------------------- Constants --------------------------------*/
 let userName = ""
 let secondScreenh1 = "The Crash"
-
+let answerToTheFirstOption = '';
 
 
 
@@ -24,7 +29,7 @@ addNameButton.textContent = "Next"
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+buttonContainer.classList.add = 'options'
 
 
 
@@ -52,6 +57,7 @@ function handleGetUserName() {
     isNameAdded()
 }
 const clearPage = () => {
+    //this is to remove everything inside the body
     document.body.innerHTML = ""
 } 
 const isNameAdded = () => {
@@ -60,6 +66,95 @@ const isNameAdded = () => {
         secondScreen()
     }
 };
+
+const handleFirstAnswer = (event) => {
+    
+    const selectedOptionValue = event.target.id
+    
+    if(selectedOptionValue === 'firstOption1') {
+        answerToTheFirstOption = selectedOptionValue
+        isFirstQuestionAnswered(selectedOptionValue)
+    } else if(selectedOptionValue === 'secondOption1') {
+        answerToTheFirstOption = selectedOptionValue
+        isFirstQuestionAnswered(selectedOptionValue)
+    }
+    
+}
+
+const isFirstQuestionAnswered = (answerToTheFirstOptionParameter) => {
+    
+    if(answerToTheFirstOptionParameter ===  'firstOption1') {
+        clearPage()
+        //go the next step
+        page1h1Element.innerText = `Search for Oxygen`
+        
+        pageh2Element.innerText = 
+        `${userName} you find the oxygen tank, but night is approaching faster than expected
+        do you leave the wreckage to search for shelter or stay inside ?`
+        
+
+
+        h3Question.innerHTML = 'What do you do second ? leave to find shelter stay in the wreckage'
+
+
+        
+
+        bodyElement.appendChild(page1h1Element)
+        bodyElement.appendChild(pageh2Element)
+        bodyElement.appendChild(h3Question)
+
+        handleOptionCreation('leave shelter', 'stay in Wreckage', 'firstOption2', 'secondOption2')
+
+
+    }
+}
+
+const isSecondQuestionAnswered = () => {
+
+    if(answerToTheSecondOptionParameter ===  'firstOption2') {
+        clearPage()
+        //go the next step
+        page1h1Element.innerText = `Alien Creature`
+        
+        pageh2Element.innerText = 
+        `${userName} You encounter dangerous alien creatures in the distance but find a cave.`
+        
+
+
+        h3Question.innerHTML = 'What do you do second ? leave to find shelter stay in the wreckage'
+
+
+        
+
+        bodyElement.appendChild(page1h1Element)
+        bodyElement.appendChild(pageh2Element)
+        bodyElement.appendChild(h3Question)
+
+        handleOptionCreation('leave shelter', 'stay in Wreckage', 'firstOption2', 'secondOption2')
+
+
+    }
+}
+
+const handleOptionCreation = (optionButton1Text, optionButton2Text, firstChoice, secondChoice) => {
+    
+
+
+    //optionButton1.innerText = 'Search the Wreckage'
+    optionButton1.innerText = optionButton1Text
+    //optionButton2.innerText = 'Exit Wreckage'
+    optionButton2.innerText = optionButton2Text
+
+    optionButton1.setAttribute('id', firstChoice)
+    optionButton2.setAttribute('id', secondChoice)
+
+    buttonContainer.appendChild(optionButton1)
+    buttonContainer.appendChild(optionButton2)
+
+    bodyElement.appendChild(buttonContainer)
+
+}
+
 const secondScreen = () => {
  page1h1Element.innerText = secondScreenh1   
 bodyElement.appendChild(page1h1Element)
@@ -67,7 +162,17 @@ const secondScreenPara = document.createElement("p")
 secondScreenPara.innerText = `You wake up in your wrecked spaceship ${userName}, alarms blaring,
 with limited time to act.`
 bodyElement.appendChild(secondScreenPara)
-console.log(userName, "dvsv")
+bodyElement.append(h3Question.innerHTML = 'What do you do first \n')
+
+
+handleOptionCreation('Search the Wreckage', 'Exit Wreckage', 'firstOption1', 'secondOption1')
+
+
+
+
+
+buttonContainer.addEventListener('click', handleFirstAnswer)
+console.log(buttonContainer)
 
 }
 
@@ -79,3 +184,5 @@ firstScreen()
 handleGetUserName()
 // clearPage()
 isNameAdded()
+isFirstQuestionAnswered()
+isSecondQuestionAnswered()
